759d9417dea267bfd7ed708ff7f34d691edcbe0c
William <wreyesmartinez4361@conestogac.on.ca>
2019-06-16 00:01:00 -0400

    triangle validation

--- SqaAssignment2/TriangleSolver.cs
+++ SqaAssignment2/TriangleSolver.cs
@@ -9,11 +9,47 @@ namespace SqaAssignment2
     static class TriangleSolver
     {
 
-        public static string Analyze(int sideOne, int sideTwo, int sideThree)
-        {
-            string typeTriangle = string.Empty;
+        public static int sideOne;
+        public static int sideTwo;
+        public static int sideThree;
 
+        public static string Analyze(int One, int Two, int Three)
+        {
+            sideOne = One;
+            sideTwo = Two;
+            sideThree = Three;
+
+            string typeTriangle = string.Empty;
+            
+            if (validateNumbers())
+                if (triangleInequalityTheorem())
+                {
+                    if ((sideOne == sideTwo) && (sideOne == sideThree))
+                        typeTriangle = "Equilateral";
+                    else if( (sideOne == sideTwo) || (sideOne == sideThree) || (sideTwo == sideThree))
+                        typeTriangle = "Isosceles";
+                    else if ((sideOne != sideTwo) || (sideOne != sideThree) || (sideTwo != sideThree))
+                        typeTriangle = "Scalene";
+                }
             return typeTriangle;
+        }
+
+        public static bool validateNumbers()
+        {
+            if (sideOne > 0 && sideTwo > 0 && sideThree > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool triangleInequalityTheorem()
+        {
+            if ( (sideOne + sideTwo)   > sideThree &&
+                 (sideOne + sideThree) > sideTwo   &&
+                 (sideTwo + sideThree) > sideOne)
+                return true;
+            else
+                return false;
         }
 
     }
